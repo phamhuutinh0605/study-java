1ccbfaebdedac5e424cd182514016eb98ec4dfec
tinh-sds <phamhuutinh801@gmail.com>
2026-06-01 16:45:14 +0700

    re-commit

--- src/main/java/com/study_java/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/study_java/common/exception/GlobalExceptionHandler.java
@@ -36,7 +36,6 @@ public class GlobalExceptionHandler {
                 ));
     }
 
-
     @ExceptionHandler(NoHandlerFoundException.class)
     public ResponseEntity<ApiErrorResponse> handleNotFound(
             NoHandlerFoundException ex
